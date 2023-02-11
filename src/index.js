1580e33b292f6f8fe7a1f8353394eff6a2f13b7e
Roman.P <killme15139214@gmail.com>
2023-02-11 21:05:53 +0300

    add: quotes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./main.scss";
 
 import timeSlider from "./js/imageSlider";
 import greeting from "./js/greeting";
+import quotes from "./js/quotes";
---
+++ src/js/quotes.js
@@ -0,0 +1,35 @@
+const quote = document.querySelector(".quote");
+const quote_refresh = document.querySelector(".quote_refresh");
+
+const getQuote = async () => {
+  const res = await fetch(
+    "https://cors.eu.org/https://api.forismatic.com/api/1.0/",
+    {
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      body: "method=getQuote&format=json&lang=ru",
+      method: "POST",
+    }
+  );
+  const data = await res.json();
+  console.log(data);
+  setTimeout(() => {
+    document.querySelector(".quote_text").textContent = data.quoteText;
+    document.querySelector(".quote_author").textContent = data.quoteAuthor;
+  }, 100);
+};
+getQuote();
+
+quote_refresh.addEventListener("click", () => {
+  quote_refresh.classList.add("active");
+  getQuote();
+});
+
+quote_refresh.addEventListener(
+  "animationend",
+  () => {
+    quote_refresh.classList.remove("active");
+  },
+  false
+);
+
+export default getQuote;
