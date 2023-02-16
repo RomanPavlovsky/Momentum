d7acf2934900e47b92f07a95e432a7f9b6c5c817
Roman.P <killme15139214@gmail.com>
2023-02-16 17:59:44 +0300

    feat: add settings modals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "./main.scss";
-
 import timeSlider from "./js/imageSlider";
 import greeting from "./js/greeting";
 import quotes from "./js/quotes";
 import userWeather from "./js/userWeather";
 import audioPlayer from "./js/audioPlayer";
-import settings from "./js/settings";
+import settings from "./js/settingsModal";
