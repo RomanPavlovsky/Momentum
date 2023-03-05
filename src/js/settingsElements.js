35f6882d8d07dc259406ad016f0c1198c6a265d6
Roman.P <killme15139214@gmail.com>
2023-03-05 20:17:24 +0300

    fix elements settings

--- src/js/settingsElements.js
+++ src/js/settingsElements.js
@@ -14,29 +14,47 @@ const audioplayer = document.querySelector(".audioplayer__wrapper");
 const weather = document.querySelector(".weather__wrapper");
 const quote = document.querySelector(".quote__wrapper");
 const greeting = document.querySelector(".greeting__wrapper");
-const toggleElements = () => {
-  audioToggle.addEventListener("click", () => {
-    audioToggle.checked === true
-      ? (audioplayer.style.display = "flex")
-      : (audioplayer.style.display = "none");
-  });
-  weatherToggle.addEventListener("click", () => {
-    weatherToggle.checked === true
-      ? (weather.style.display = "flex")
-      : (weather.style.display = "none");
-  });
-  quoteToggle.addEventListener("click", () => {
-    quoteToggle.checked === true
-      ? (quote.style.display = "flex")
-      : (quote.style.display = "none");
-  });
-  greetingToggle.addEventListener("click", () => {
-    greetingToggle.checked === true
-      ? (greeting.style.display = "flex")
-      : (greeting.style.display = "none");
-  });
-};
+const allToggle = document.querySelectorAll(".elements-window .toggle__input");
 
+let elementsSettings = {};
+const getElemSettings = () => {
+  if (localStorage.elements === undefined) {
+    allToggle.forEach((elem) => {
+      elementsSettings[elem.name] = elem.checked;
+    });
+    localStorage.elements = JSON.stringify(elementsSettings);
+  } else {
+    elementsSettings = JSON.parse(localStorage.elements);
+    allToggle.forEach((elem) => {
+      elem.checked = elementsSettings[elem.name];
+    });
+  }
+};
+getElemSettings();
+setInterval(() => {
+  elementsSettings.player === true
+    ? (audioplayer.style.display = "flex")
+    : (audioplayer.style.display = "none");
+  elementsSettings.weather === true
+    ? (weather.style.display = "flex")
+    : (weather.style.display = "none");
+  elementsSettings.quote === true
+    ? (quote.style.display = "flex")
+    : (quote.style.display = "none");
+  elementsSettings.greeting === true
+    ? (greeting.style.display = "flex")
+    : (greeting.style.display = "none");
+});
+function setElementStorage() {
+  elementsSettings[this.name] = this.checked;
+  localStorage.elements = JSON.stringify(elementsSettings);
+}
+const toggleElements = () => {
+  audioToggle.addEventListener("click", setElementStorage);
+  weatherToggle.addEventListener("click", setElementStorage);
+  quoteToggle.addEventListener("click", setElementStorage);
+  greetingToggle.addEventListener("click", setElementStorage);
+};
 toggleElements();
 
 export default toggleElements;
